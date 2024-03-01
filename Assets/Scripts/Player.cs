f862fb25749ab9072a7898da3d411c100fc16c35
Adam Graham <adam@zigurous.com>
2024-03-01 16:41:12 -0600

    Replace FindObjectOfType with GameManager.Instance

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,7 +38,7 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Obstacle")) {
-            FindObjectOfType<GameManager>().GameOver();
+            GameManager.Instance.GameOver();
         }
     }
 
